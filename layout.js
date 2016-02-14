fa7b60aaa873eb4dfc2aed0e08dc545df1f933e7
anvaka <anvaka@gmail.com>
2016-02-13 22:33:50 -0800

    Fixed help text

--- layout.js
+++ layout.js
@@ -1,6 +1,6 @@
 console.log('Loading graph');
 var graph = require('./lib/loadgraph.js')();
-console.log('Loaded ' + graph.getNodesCount() + ' nodes; ' + graph.getLinksCount() + ' packages');
+console.log('Loaded ' + graph.getNodesCount() + ' nodes; ' + graph.getLinksCount() + ' edges');
 
 var createLayout = require('ngraph.offline.layout');
 var layout = createLayout(graph);
